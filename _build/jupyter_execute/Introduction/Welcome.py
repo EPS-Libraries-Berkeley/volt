f3e0bf9fdf86435bb1bff7b22b0873edcf98f440
Sam Teplitzky <steplitz@library.berkeley.edu>
2020-06-17 15:51:08 -0700

    Troubleshooting

---
+++ _build/jupyter_execute/Introduction/Welcome.py
@@ -0,0 +1,34 @@
+# Welcome
+
+Welcome to the Engineering & Physical Sciences Libraries.
+
+Follow these tutorials...
+
+
+
+
+```{toctree}
+:hidden:
+:titlesonly:
+:caption: Research Workflows
+
+../Research_Workflows/Introduction_Research_Workflows
+```
+
+
+```{toctree}
+:hidden:
+:titlesonly:
+:caption: LaTeX
+
+../LaTeX/Intro_to_LaTeX
+```
+
+
+```{toctree}
+:hidden:
+:titlesonly:
+:caption: Other
+
+../README.md
+```
